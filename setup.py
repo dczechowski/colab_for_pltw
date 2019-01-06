be12eec82d8be8d5cc2e6483c6c291b434e78710
Mr Czechowski <davidczechowski@hpcsd.org>
2019-01-06 01:06:48 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='colab_for_pltw',
+      version='0.1',
+      description='Colab for PLTW CSP',
+      url='http://github.com/dczechowski/colab_for_pltw',
+      author='dczechowski',
+      author_email='davidczechowski@hpcsd.org',
+      license='TBD',
+      packages=['colab_for_pltw'],
+      zip_safe=False)
